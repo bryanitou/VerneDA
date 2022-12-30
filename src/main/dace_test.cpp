5ce85d3fddac4ee2c1faf7d2e6f8793c4b1b17ea
Bryan Cabrera <bryangcr25@gmail.com>
2022-12-30 12:33:55 +0100

    Create main entry point.

--- src/main/dace_test.cpp
+++ src/main/dace_test.cpp
@@ -2,4 +2,14 @@
  * First c++ main to interact with the 3rdparty.
  */
 
+// System libraries
+#include <iostream>
+#include <filesystem>
 
+/**
+ * Main entry point
+ */
+int main(int argc, char* argv[])
+{
+
+}
