b20032f073b5de191b52cd1203c6f6128a7a33c8
Bryan Cabrera <bryangcr25@gmail.com>
2022-12-30 13:21:33 +0100

    Testing tutorials.

--- src/main/dace_test.cpp
+++ src/main/dace_test.cpp
@@ -3,13 +3,26 @@
  */
 
 // System libraries
+#include <cmath>
 #include <iostream>
-#include <filesystem>
+#include <fstream>
+#include <iomanip>
+
+// DACE library
+#include <dace/dace.h>
 
 /**
  * Main entry point
  */
 int main(int argc, char* argv[])
 {
+    DACE::DA::init( 20, 1 ); // initialize DACE for 20th-order computations in 1 variable
 
+    DACE::DA x = DACE::DA(1); // initialize x as DA
+
+    DACE::DA y = sin(x); // compute y = sin(x)
+
+    // print x and y to screen
+    std::cout << "x" << std::endl << x << std::endl;
+    std::cout << "y = sin(x)" << std::endl << y;
 }
