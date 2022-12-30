b62a2ae50a636815eff5bcd73a85912981c0b42d
Bryan Cabrera <bryangcr25@gmail.com>
2022-12-30 18:50:39 +0100

    Second example where the trigonometric functions (squared) are presented.

---
+++ src/main/Example2-dace_test.cpp
@@ -0,0 +1,67 @@
+/**
+ * First c++ main to interact with the 3rdparty.
+ */
+
+// System libraries
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+// DACE library
+#include <dace/dace.h>
+
+
+// Forward declarate stuff
+void dump_variables(DACE::DA &x, DACE::DA &y, const std::string & file_path);
+
+/**
+ * Main entry point
+ */
+int main(int argc, char* argv[])
+{
+    // Init variable, 20-th computations and 1 variable
+    DACE::DA::init( 20, 1 );
+
+    // Initialize x as DA
+    DACE::DA x = DACE::DA(1);
+
+    // Compute sin(x)^2
+    DACE::DA y1 = DACE::sqr(sin(x));
+
+    // Info
+    std::cout << "sin(x)^2" << std::endl << y1 << std::endl;
+
+    // Compute cos(x)^2
+    DACE::DA y2 = DACE::sqr(cos(x));
+
+    // Info
+    std::cout << "cos(x)^2" << std::endl << y2 << std::endl;
+
+    // Compute and print sin(x)^2+cos(x)^2
+    std::cout << "sin(x)^2+cos(x)^2" << std::endl << y1+y2 << std::endl;
+
+    // dump_variables(x, y, "./out/Example2-dace_test.txt");
+}
+
+void dump_variables(DACE::DA &x, DACE::DA &y, const std::string & file_path)
+{
+    // Create the file stream
+    std::ofstream file2write;
+    file2write.open(file_path);
+
+    // Safety check
+    bool same_length = x.size() == y.size();
+
+    // Check bool
+    if (same_length) {
+        // Do something here
+    }
+
+    // Easy print
+    file2write << "x" << std::endl << x << std::endl;
+    file2write << "y = sin(x)" << std::endl << y;
+
+    // Close the stream
+    file2write.close();
+}
