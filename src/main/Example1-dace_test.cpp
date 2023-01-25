22599f8d7082932747b5673f9e86504065b45f21
Bryan Cabrera <bryangcr25@gmail.com>
2023-01-25 19:29:36 +0100

    Create dedicated space for tooling functions.

--- src/main/Example1-dace_test.cpp
+++ src/main/Example1-dace_test.cpp
@@ -11,9 +11,8 @@
 // DACE library
 #include <dace/dace.h>
 
-
-// Forward declarate stuff
-void dump_variables(DACE::DA &x, DACE::DA &y, const std::string & file_path);
+// Project libraries
+#include "tools.h"
 
 /**
  * Main entry point
@@ -33,31 +32,6 @@ int main(int argc, char* argv[])
     std::cout << "x" << std::endl << x << std::endl;
     std::cout << "y = sin(x)" << std::endl << y;
 
-    dump_variables(x, y, "./out/Example1-dace_test.txt");
-}
-
-void dump_variables(DACE::DA &x, DACE::DA &y, const std::string & file_path)
-{
-    // Create the file stream
-    std::ofstream file2write;
-    file2write.open(file_path);
-
-    // Safety check
-    bool same_length = x.size() == y.size();
-
-    // Check bool
-    if (same_length) {
-        // Do something here
-    }
-
-    // Easy print
-    //file2write << "x" << std::endl << x << std::endl;
-    file2write << "y = sin(x)" << std::endl << y;
-
-    // TODO: Try to print coef, exponents (and order) in a organized way so we can print this using Taylor Expansions
-    // TODO: later in python?
-    // for (int i = 0; i < y.getMaxOrder())
-
-    // Close the stream
-    file2write.close();
+    // Dump variables
+    tools::dump_variables(y, "./out/Example1-dace_test.txt");
 }
--- src/main/Example2-dace_test.cpp
+++ src/main/Example2-dace_test.cpp
@@ -11,9 +11,8 @@
 // DACE library
 #include <dace/dace.h>
 
-
-// Forward declarate stuff
-void dump_variables(DACE::DA &x, DACE::DA &y, const std::string & file_path);
+// Project libraries
+#include "tools.h"
 
 /**
  * Main entry point
@@ -41,27 +40,5 @@ int main(int argc, char* argv[])
     // Compute and print sin(x)^2+cos(x)^2
     std::cout << "sin(x)^2+cos(x)^2" << std::endl << y1+y2 << std::endl;
 
-    dump_variables(x, y1, "./out/Example2-dace_test.txt");
-}
-
-void dump_variables(DACE::DA &x, DACE::DA &y, const std::string & file_path)
-{
-    // Create the file stream
-    std::ofstream file2write;
-    file2write.open(file_path);
-
-    // Safety check
-    bool same_length = x.size() == y.size();
-
-    // Check bool
-    if (same_length) {
-        // Do something here
-    }
-
-    // Easy print
-    // file2write << "x" << std::endl << x << std::endl;
-    file2write << "y = sin(x)^2" << std::endl << y.;
-
-    // Close the stream
-    file2write.close();
+    tools::dump_variables(y1, "./out/Example2-dace_test.txt");
 }
