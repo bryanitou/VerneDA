1aabd90875f78e9a285068549dd2dfdbf5041dae
Bryan Cabrera <bryangcr25@gmail.com>
2023-03-23 16:56:35 +0100

    Sphere plotter for debugging.

---
+++ common/tools-py/sphere.py
@@ -0,0 +1,20 @@
+import reader
+import banana
+
+
+def main():
+    # File to assess
+    filepath = "/home/bryan/CLionProjects/ISAE/research_project/getting2knowDace/out/attp/non_eval_deltas_expression.dd"
+    output = filepath.replace(".dd", "-Sphere.png")
+    # Read and get results
+    dd_dict = reader.read_dd_file(filepath, verbose=True)
+
+    # Now, call to plot
+    v = banana.get_vector(dd_dict, idx=3, verbose=True)
+
+    # Use quaternion plotter
+    banana.plot_quaternion(v[0], v[1], v[2], v[3], unit_str="rad", output=output)
+
+
+if __name__ == '__main__':
+    main()
