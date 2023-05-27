ecaba0ff2e4153b5d5e8b00c8c2f694e46f3c5cf
Bryan Cabrera <bryangcr25@gmail.com>
2023-05-27 21:08:56 +0200

    Formatting.

--- src/core/tools/ep.h
+++ src/core/tools/ep.h
@@ -15,39 +15,39 @@
 #include "base/enums.h"
 
 namespace tools::enums
-    {
-        /**
-         * Parse VELOCITY enum to string.
-         * @param velocity
-         * @return
-         */
-        [[maybe_unused]] std::string VELOCITY2str(VELOCITY velocity);
+{
+    /**
+     * Parse VELOCITY enum to string.
+     * @param velocity
+     * @return
+     */
+    [[maybe_unused]] std::string VELOCITY2str(VELOCITY velocity);
 
-        /**
-         * Parse POSITION enum to string.
-         * @param position
-         * @return
-         */
-        [[maybe_unused]] std::string POSITION2str(POSITION position);
+    /**
+     * Parse POSITION enum to string.
+     * @param position
+     * @return
+     */
+    [[maybe_unused]] std::string POSITION2str(POSITION position);
 
-        /**
-         * Parse DISTRIBUTION enum to string.
-         * @param distribution
-         * @return
-         */
-        std::string DISTRIBUTION2str(DISTRIBUTION distribution);
+    /**
+     * Parse DISTRIBUTION enum to string.
+     * @param distribution
+     * @return
+     */
+    std::string DISTRIBUTION2str(DISTRIBUTION distribution);
 
-        /**
-         * Parse STATE CONTROL VECTOR to VELOCITY enum.
-         * @param state
-         * @return VELOCITY enum
-         */
-        VELOCITY STATE2VELOCITY(STATE state);
+    /**
+     * Parse STATE CONTROL VECTOR to VELOCITY enum.
+     * @param state
+     * @return VELOCITY enum
+     */
+    VELOCITY STATE2VELOCITY(STATE state);
 
-        /**
-        * Parse STATE CONTROL VECTOR to POSITION enum.
-        * @param state
-        * @return POSITION enum
-        */
-        POSITION STATE2POSITION(STATE state);
-    };
+    /**
+    * Parse STATE CONTROL VECTOR to POSITION enum.
+    * @param state
+    * @return POSITION enum
+    */
+    POSITION STATE2POSITION(STATE state);
+};
