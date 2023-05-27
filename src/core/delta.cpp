020ba8107742443eef2dbb005966d76940f0c442
Bryan Cabrera <bryangcr25@gmail.com>
2023-05-27 21:09:11 +0200

    Sample in euler domain.

--- src/core/delta.cpp
+++ src/core/delta.cpp
@@ -131,10 +131,9 @@ void delta::generate_gaussian_deltas(int n)
             }
 
             new_delta = {
-                    nq[0] - this->mean_quaternion_[0],
-                    nq[1] - this->mean_quaternion_[1],
-                    nq[2] - this->mean_quaternion_[2],
-                    nq[3] - this->mean_quaternion_[3],
+                    stddevs_distr[0](generator),
+                    stddevs_distr[1](generator),
+                    stddevs_distr[2](generator),
                     stddevs_distr[3](generator),
                     stddevs_distr[4](generator),
                     stddevs_distr[5](generator)};
