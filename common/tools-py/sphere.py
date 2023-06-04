68ad2878e6d4b2c5c0132b0cb5ce3af6c8362a0e
Bryan Cabrera <bryangcr25@gmail.com>
2023-06-04 17:28:35 +0200

    Misc. changes in debugging plotter.

--- common/tools-py/sphere.py
+++ common/tools-py/sphere.py
@@ -1,4 +1,6 @@
 #!env/python
+import matplotlib.pyplot
+
 import reader
 import banana
 
@@ -8,7 +10,7 @@ import quaternion_histogram
 
 def main():
     # File to assess
-    filepath = "./out/example/attitude2/non_eval_deltas_expression.dd"
+    filepath = "./out/example/attitude_test/non_eval_deltas_expression.dd"
     output = filepath.replace(".dd", "-Sphere.pdf")
 
     # Read and get results
@@ -20,6 +22,10 @@ def main():
     # Fix quaternion
     # TODO: find a proper definition for this step
     v[0] = [w + 1 for w in v[0]]
+
+    # Plot 3D scatter
+    # banana.plot_3d_vectors(v[0], v[1], v[2], unit_str="-", output=output)
+    # banana.plot_3d_scatter(v[0], v[1], v[2], unit_str="rad", output=output)
 
     # Use quaternion plotter
     banana.plot_quaternion(v[0], v[1], v[2], v[3], unit_str="-", output=output)
