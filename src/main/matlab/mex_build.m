9c14d17cf32cd10ef22396c8de58aeaf7524375f
Bryan Cabrera <bryangcr25@gmail.com>
2023-11-11 13:17:53 +0100

    Mex compiling with -RPATH linking.

--- src/main/matlab/mex_build.m
+++ src/main/matlab/mex_build.m
@@ -1,10 +1,13 @@
 % Set mex file
-dace_include = '-Ibuild/VerneDA-install-debug/lib/dace/include';
-dace_lib_dir = '-Lbuild/VerneDA-install-debug/lib/dace/lib';
-verne_include = '-Isrc/core';
-verne_lib_dir = '-Lbuild/VerneDA-install-debug/lib';
+dace_lib_dir = 'build/VerneDA-install-debug/lib/dace/lib';
+dace_include_arg = '-Ibuild/VerneDA-install-debug/lib/dace/include';
+dace_lib_dir_arg = strcat('-L', dace_lib_dir);
+verne_lib_dir = 'build/VerneDA-install-debug/lib';
+verne_include_arg = '-Isrc/core';
+verne_lib_dir_arg =  strcat('-L', verne_lib_dir);
 mex_file = 'src/main/mex_vsaod.cpp';
 CXXFLAGS = 'CXXFLAGS=$CXXFLAGS -std=c++20 -Wall';
+LDFLAGS = strcat('LDFLAGS=$LDFLAGS -Wl,-rpath,', dace_lib_dir,',-rpath,', verne_lib_dir);
 CXX = 'CXX=/usr/bin/g++';
 outdir = 'mex';
 
@@ -14,4 +17,4 @@ if ~exist(outdir, 'dir')
 end
 
 % Build mex file
-mex('-v', dace_include, dace_lib_dir, '-ldace', verne_include, verne_lib_dir, '-lcore', '-lads', '-ltools', mex_file, '-outdir', outdir, CXX, CXXFLAGS)
+mex('-v', dace_include_arg, dace_lib_dir_arg, '-ldace', verne_include_arg, verne_lib_dir_arg, '-lcore', '-lads', '-ltools', mex_file, '-outdir', outdir, CXX, CXXFLAGS, LDFLAGS)
