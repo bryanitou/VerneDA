ddaeed67336d592b90682d6a2d2072508fc65a57
Bryan Cabrera <bryangcr25@gmail.com>
2023-12-17 21:18:44 +0100

    Returning simple DA propagation to matlab.

--- src/main/mex_vsaod.cpp
+++ src/main/mex_vsaod.cpp
@@ -17,6 +17,18 @@
 # define dace_array std::vector<DACE::AlgebraicVector<double>>
 
 class MexFunction : public matlab::mex::Function {
+
+private:
+    // Get pointer to engine
+    std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+
+    // Get array factory
+    matlab::data::ArrayFactory factory;
+
+    // Set some variables used in the functions
+    std::vector<double> betas;
+    DACE::AlgebraicVector<DACE::DA> scv0;
+
 public:
     void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) override
     {
@@ -46,13 +58,33 @@ public:
                 inertia = convertMatlab3x3Array2Normal3x3Array(inputs[8]);
             }
 
-            // Perform operation
-            auto fin_state = propagate_orbit_loads(ini_state, stddev, t, ci, nli, n_max, n_samples, str_enum, inertia);
+            // Simple DA propagation
+            if (n_max == -1)
+            {
+              // Info
+              matlabPtr->feval(u"fprintf",
+                               0,
+                               std::vector<matlab::data::Array>(
+                                       {factory.createScalar("DA Simple propagation selected...\n")}));
 
-            // Convert all states to matlab array
-            outputs[0] = convertNormalVector2MatlabTypedArray(*fin_state);
+              // Perform operation
+              auto fin_state = propagate_orbit_get_final_DA(ini_state, stddev, t, ci, nli, n_max, n_samples, str_enum, inertia);
+
+              // Convert all states to matlab array
+              outputs[0] = convertNormalStr2MatlabStr(fin_state);
+            }
+            else // LOADS propagation
+            {
+              // Perform operation
+              auto fin_state = propagate_orbit_loads(ini_state, stddev, t, ci, nli, n_max, n_samples, str_enum, inertia);
+
+              // Convert all states to matlab array
+              outputs[0] = convertNormalVector2MatlabTypedArray(*fin_state);
+            }
+
+
         }
-        catch (int )
+        catch (int)
         {
             std::fprintf(stdout, "ERROR: Something unexpected happened...");
         }
@@ -62,12 +94,6 @@ public:
         // Auxiliary variable
         std::string err2print;
         bool print_error = false;
-
-        // Get pointer to engine
-        std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
-
-        // Get array factory
-        matlab::data::ArrayFactory factory;
 
         // Define vectors to check the inputs
         std::vector<int> vectors2check6 = {0, 1};
@@ -287,15 +313,39 @@ public:
         return std::string(str2convert[0]);
     }
 
+    template <typename T>
+    std::string toUTF8(const  std::basic_string<T,  std::char_traits<T>,  std::allocator<T>>& source)
+    {
+      std::string result;
+
+      std::wstring_convert< std::codecvt_utf8_utf16<T>, T> convertor;
+      result = convertor.to_bytes(source);
+
+      return result;
+    }
+
+    template <typename T>
+    void fromUTF8(const std::string& source, std::basic_string<T,std::char_traits<T>, std::allocator<T>>& result)
+    {
+      std::wstring_convert<std::codecvt_utf8_utf16<T>, T> convertor;
+      result = convertor.from_bytes(source);
+    }
+
+    matlab::data::TypedArray<matlab::data::MATLABString> convertNormalStr2MatlabStr(std::string & c_plus_plus_string)
+    {
+      std::u16string u16_String{};
+
+      fromUTF8(c_plus_plus_string, u16_String);
+
+      // Return result
+      return factory.createArray<matlab::data::MATLABString>({1, 1},
+                                                             {matlab::data::MATLABString(u16_String.c_str()) });
+    }
+
     double* convertMatlab3x3Array2Normal3x3Array(const matlab::data::TypedArray<double>& arr2convert)
     {
         double* result = (double*) malloc(sizeof (double ) * 3 * 3);
 
-        // Get pointer to engine
-        std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
-
-        // Get array factory
-        matlab::data::ArrayFactory factory;
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
@@ -317,13 +367,10 @@ public:
     }
 
 
-    static matlab::data::TypedArray<double> convertNormalVector2MatlabTypedArray(const dace_array& final_state)
+    matlab::data::TypedArray<double> convertNormalVector2MatlabTypedArray(const dace_array& final_state)
     {
-        // Get array factory
-        matlab::data::ArrayFactory factory;
-
         // Set result
-        matlab::data::TypedArray<double> result = factory.createArray<double>({final_state[0].size(), final_state.size()});
+        matlab::data::TypedArray<double> result = this->factory.createArray<double>({final_state[0].size(), final_state.size()});
 
         // Iterate and assign
         for (int i = 0; i < final_state.size(); i++)
@@ -338,150 +385,171 @@ public:
         return result;
     }
 
+    SuperManifold* propagate_orbit_loads_SUPER_MANIFOLD(const std::vector<double>& t, double& nli, int& n_max, PROBLEM prob, const double* inertia = nullptr, bool simple_DA_propagation = false)
+    {
+      matlabPtr->feval(u"fprintf",
+                       0,
+                       std::vector<matlab::data::Array>({factory.createScalar(scv0.toString())}));
+
+      // Initial and final time and time step
+      double const t0 = t[0];
+      double const tf = t[1];
+      double const dt = t[2];
+
+      // Initialize integrator
+      auto objIntegrator = std::make_unique<integrator>(INTEGRATOR::RK4, simple_DA_propagation ? ALGORITHM::NONE : ALGORITHM::LOADS , dt);
+
+      // Build super manifold: LOADS
+      auto super_manifold = new SuperManifold(nli, n_max, simple_DA_propagation ? ALGORITHM::NONE : ALGORITHM::LOADS );
+
+      // Set beta constant in integrator
+      objIntegrator->set_beta(const_cast<std::vector<double> &>(betas));
+
+      // Initialize problem
+      problems * problem;
+
+      if (prob == PROBLEM::TWO_BODY)
+      {
+        problem = new problems(prob, 1);
+      }
+      else if (prob == PROBLEM::FREE_TORQUE_MOTION)
+      {
+        // Free Torque Motion problem
+        problem = new problems(prob);
+
+        // Create double[3][3]
+        double inertia3x3[3][3];
+
+        for (int i = 0; i < 3; i++)
+        {
+          for (int j = 0; j < 3; j++)
+          {
+            // Pass values
+            inertia3x3[i][j] = inertia[i*3 + j];
+
+            // Prepare string
+            auto err2print = tools::string::print2string("i: %d, j: %d --> %f\n", i, j, inertia3x3[i][j]);
+
+            // Show error
+            matlabPtr->feval(u"fprintf",
+                             0,
+                             std::vector<matlab::data::Array>(
+                                     {factory.createScalar(err2print)}));
+          }
+        }
+
+        // Set the inertia matrix in problem object
+        problem->set_inertia_matrix(inertia3x3);
+      }
+      else
+      {
+        // Throw error
+      }
+
+      // Deduce whether interruption should be made or not
+      auto interruption = true;
+
+      // Set problem ptr in the integrator
+      objIntegrator->set_problem_ptr(problem);
+
+      // Setting integrator parameters
+      objIntegrator->set_integration_parameters(scv0, t0, tf,interruption);
+
+      // Set integrator in the super manifold
+      super_manifold->set_integrator_ptr(objIntegrator.get());
+
+      // Docu: Set new truncation error and get the previous one
+      double new_eps = 1e-40;
+      DACE::DA::setEps(new_eps);
+
+      // Apply main algorithm: ADS / LOADS. And integration algorithm
+      // std::string prop_summary{};
+      // super_manifold->split_domain(&prop_summary);
+      super_manifold->split_domain();
+
+      return super_manifold;
+    }
+
+    void initialize_initial_conditions(const std::vector<double>& ini_state, const std::vector<double>& stddev, double& ci)
+    {
+      // Initialize DACE with 6 variables
+      DACE::DA::init(2, ini_state.size());
+
+      // Initialize beta
+      this->betas = std::vector<double>(stddev.size());
+      for (int i = 0; i < stddev.size(); i++)
+        betas[i] = ci * stddev[i];
+
+      // Initialize state DA vector
+      this->scv0 = DACE::AlgebraicVector<DACE::DA>(ini_state.size());
+      for (int i = 0; i < ini_state.size(); i++)
+        scv0[i] = ini_state[i] + betas[i] * DACE::DA(i + 1);
+    }
+
+    std::string propagate_orbit_get_final_DA(const std::vector<double>& ini_state, const std::vector<double>& stddev, const std::vector<double>& t, double& ci, double& nli, int& n_max, int& n_samples, PROBLEM prob, const double* inertia = nullptr)
+    {
+      // Initialize initial conditions
+      this->initialize_initial_conditions(ini_state, stddev, ci);
+
+      // Get super manifold
+      auto super_manifold = propagate_orbit_loads_SUPER_MANIFOLD(t, nli,  n_max, prob, inertia);
+
+      // Return result
+      return super_manifold->current_->front().toString();
+    }
+
     std::shared_ptr<dace_array> propagate_orbit_loads(const std::vector<double>& ini_state, const std::vector<double>& stddev, const std::vector<double>& t, double& ci, double& nli, int& n_max, int& n_samples, PROBLEM prob, const double* inertia = nullptr)
     {
-        // Get pointer to engine
-        std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+      // Initialize initial conditions
+      this->initialize_initial_conditions(ini_state, stddev, ci);
 
-        // Get array factory
-        matlab::data::ArrayFactory factory;
+      // Get super manifold
+      auto super_manifold = propagate_orbit_loads_SUPER_MANIFOLD(t, nli,  n_max, prob, inertia);
 
-        // Initialize DACE with 6 variables
-        DACE::DA::init(2, ini_state.size());
+      // Build deltas class
+      auto deltas_engine = std::make_shared<delta>();
 
-        // Initialize beta
-        std::vector<double> betas(stddev.size());
-        for (int i = 0; i < stddev.size(); i++)
-            betas[i] = ci * stddev[i];
+      // Set quaternion distribution stuff in case of FTMP
+      if (prob == PROBLEM::FREE_TORQUE_MOTION)
+      {
+        // Set some options
+        deltas_engine->set_bool_option(DELTA_GENERATOR_OPTION::ATTITUDE, true);
+        deltas_engine->set_bool_option(DELTA_GENERATOR_OPTION::QUAT2EULER, true);
+        deltas_engine->set_sampling_option(QUATERNION_SAMPLING::OMPL_GAUSSIAN);
+        deltas_engine->set_mean_quaternion_option(scv0.extract(0, 3).cons());
+      }
 
-        // Initialize state DA vector
-        DACE::AlgebraicVector<DACE::DA> scv0(ini_state.size());
-        for (int i = 0; i < ini_state.size(); i++)
-            scv0[i] = ini_state[i] + betas[i] * DACE::DA(i + 1);
+      // Set distribution
+      if (prob == PROBLEM::FREE_TORQUE_MOTION)
+      {
+        deltas_engine->set_stddevs_q(stddev);
+      }
+      else if (prob == PROBLEM::TWO_BODY)
+      {
+        deltas_engine->set_stddevs(stddev);
+      }
+      else
+      {
+        return {};
+      }
 
-        matlabPtr->feval(u"fprintf",
-                         0,
-                         std::vector<matlab::data::Array>({factory.createScalar(scv0.toString())}));
-
-        // Initial and final time and time step
-        double const t0 = t[0];
-        double const tf = t[1];
-        double const dt = t[2];
-
-        // Initialize integrator
-        auto objIntegrator = std::make_unique<integrator>(INTEGRATOR::RK4, ALGORITHM::LOADS, dt);
-
-        // Build super manifold: LOADS
-        auto super_manifold = new SuperManifold(nli, n_max, ALGORITHM::LOADS);
-
-        // Set beta constant in integrator
-        objIntegrator->set_beta(const_cast<std::vector<double> &>(betas));
-
-        // Initialize problem
-        problems * problem;
-
-        if (prob == PROBLEM::TWO_BODY)
-        {
-            problem = new problems(prob, 1);
-        }
-        else if (prob == PROBLEM::FREE_TORQUE_MOTION)
-        {
-            // Free Torque Motion problem
-            problem = new problems(prob);
-
-            // Create double[3][3]
-            double inertia3x3[3][3];
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    // Pass values
-                    inertia3x3[i][j] = inertia[i*3 + j];
-
-                    // Prepare string
-                    auto err2print = tools::string::print2string("i: %d, j: %d --> %f\n", i, j, inertia3x3[i][j]);
-
-                    // Show error
-                    matlabPtr->feval(u"fprintf",
-                                     0,
-                                     std::vector<matlab::data::Array>(
-                                             {factory.createScalar(err2print)}));
-                }
-            }
-
-            // Set the inertia matrix in problem object
-            problem->set_inertia_matrix(inertia3x3);
-        }
-        else
-        {
-            // Throw error
-        }
-
-        // Deduce whether interruption should be made or not
-        auto interruption = true;
-
-        // Set problem ptr in the integrator
-        objIntegrator->set_problem_ptr(problem);
-
-        // Setting integrator parameters
-        objIntegrator->set_integration_parameters(scv0, t0, tf,interruption);
-
-        // Set integrator in the super manifold
-        super_manifold->set_integrator_ptr(objIntegrator.get());
-
-        // Docu: Set new truncation error and get the previous one
-        double new_eps = 1e-40;
-        DACE::DA::setEps(new_eps);
-
-        // Apply main algorithm: ADS / LOADS. And integration algorithm
-        std::string prop_summary{};
-        super_manifold->split_domain(&prop_summary);
-
-        // Build deltas class
-        auto deltas_engine = std::make_shared<delta>();
-
-        // Set quaternion distribution stuff in case of FTMP
-        if (prob == PROBLEM::FREE_TORQUE_MOTION)
-        {
-            // Set some options
-            deltas_engine->set_bool_option(DELTA_GENERATOR_OPTION::ATTITUDE, true);
-            deltas_engine->set_bool_option(DELTA_GENERATOR_OPTION::QUAT2EULER, true);
-            deltas_engine->set_sampling_option(QUATERNION_SAMPLING::OMPL_GAUSSIAN);
-            deltas_engine->set_mean_quaternion_option(scv0.extract(0, 3).cons());
-        }
-
-        // Set distribution
-        if (prob == PROBLEM::FREE_TORQUE_MOTION)
-        {
-            deltas_engine->set_stddevs_q(stddev);
-        }
-        else if (prob == PROBLEM::TWO_BODY)
-        {
-            deltas_engine->set_stddevs(stddev);
-        }
-        else
-        {
-            return {};
-        }
-
-        // Compute deltas
-        deltas_engine->generate_deltas(DISTRIBUTION::GAUSSIAN, n_samples);
+      // Compute deltas
+      deltas_engine->generate_deltas(DISTRIBUTION::GAUSSIAN, n_samples);
 
       // Insert nominal delta, this is the center
-        deltas_engine->insert_nominal(static_cast<int>(ini_state.size()));
+      deltas_engine->insert_nominal(static_cast<int>(ini_state.size()));
 
-        // Set super manifold in deltas engine
-        deltas_engine->set_superManifold(super_manifold);
+      // Set super manifold in deltas engine
+      deltas_engine->set_superManifold(super_manifold);
 
-        // Evaluate deltas
-        deltas_engine->evaluate_deltas();
+      // Evaluate deltas
+      deltas_engine->evaluate_deltas();
 
-        // Print summary
-        std::fprintf(stdout, "%s\n", prop_summary.c_str());
-        matlabPtr->feval(u"fprintf", 0, std::vector<matlab::data::Array>({factory.createScalar(prop_summary)}));
+      // Print summary
+      // std::fprintf(stdout, "%s\n", prop_summary.c_str());
+      // matlabPtr->feval(u"fprintf", 0, std::vector<matlab::data::Array>({factory.createScalar(prop_summary)}));
 
-        // Get result
-        return deltas_engine->get_eval_deltas_poly();
+      // Get result
+      return deltas_engine->get_eval_deltas_poly();
     }
 };
