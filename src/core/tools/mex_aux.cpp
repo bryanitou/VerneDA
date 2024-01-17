89c02d6df3ad786ff77ea091781ca03e268f7a9e
Bryan Cabrera <bryangcr25@gmail.com>
2024-01-17 23:58:05 +0100

    Moved check arguments in subroutine in mex_aux.cpp

--- src/core/tools/mex_aux.cpp
+++ src/core/tools/mex_aux.cpp
@@ -1,5 +1,5 @@
 /**
- *
+ * MEX Auxiliary static functions
  */
 
 #include "mex_aux.h"
@@ -33,3 +33,184 @@ std::string mex_aux::convertMatlabStr2NormalStr(const matlab::data::TypedArray<m
     // Return result
     return std::string(str2convert[0]);
 }
+
+std::vector<std::string> mex_aux::convertMatlabStrVector2NormalStrVector(const matlab::data::TypedArray<matlab::data::MATLABString> &str2convert) {
+    auto dim = str2convert.getDimensions();
+    std::fprintf(stdout, "hi %zu\n", dim[0]);
+    std::fprintf(stdout, "hi %zu\n", dim[1]);
+
+    std::vector<std::string> result(dim[0]);
+
+    for (unsigned int i = 0; i < result.size(); i++) {result[i] = std::string(str2convert[i][0]);
+        std::fprintf(stdout, "%s\n",result[i].c_str());}
+    return result;
+}
+
+/*
+void mex_aux::checkArguments(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs, MEX_FILE_TYPE type)
+{
+    // Auxiliary variable
+    std::string err2print;
+    bool print_error = false;
+
+    // Inputs to check?
+    auto inputs2check = type == MEX_FILE_TYPE::GET_DA ? mex_aux::inputs_MEX_GET_DA() :
+            type == MEX_FILE_TYPE::VSAOD ? mex_aux::inputs_MEX_VSAOD()  : mex_aux::inputs();
+
+    // Check arguments
+    for (auto & i : inputs2check.vectors2check6)
+    {
+        // Safety check
+        if (i >= inputs.size())
+        {
+            print_error = true;
+        }
+
+        if (!print_error)
+        {
+            if (inputs[i].getType() != matlab::data::ArrayType::DOUBLE ||
+                inputs[i].getType() == matlab::data::ArrayType::COMPLEX_DOUBLE ||
+                (inputs[i].getNumberOfElements() != 6 && inputs[i].getNumberOfElements() != 7)) {
+                print_error = true;
+            }
+        }
+
+        if (print_error)
+        {
+            // String to show
+            err2print = tools::string::print2string("Input %d must be an array of 6 doubles", i + 1);
+
+            // Show string
+            std::fprintf(stderr, "ERROR: %s\n", err2print.c_str());
+        }
+    }
+
+    // Check arguments
+    for (auto & i : inputs2check.vectors2check3)
+    {
+        // Safety check
+        if (i >= inputs.size())
+        {
+            print_error = true;
+        }
+
+        if (!print_error)
+        {
+            if (inputs[i].getType() != matlab::data::ArrayType::DOUBLE ||
+                inputs[i].getType() == matlab::data::ArrayType::COMPLEX_DOUBLE ||
+                inputs[i].getNumberOfElements() != 3) {
+                print_error = true;
+            }
+        }
+
+        if (print_error)
+        {
+            // String to show
+            err2print = tools::string::print2string("Input %d must be an array of 3 doubles", i + 1);
+
+            // Show string
+            std::fprintf(stderr, "ERROR: %s\n", err2print.c_str());
+        }
+    }
+
+    // Check arguments
+    for (auto & i : inputs2check.doubles2check)
+    {
+        // Safety check
+        if (i >= inputs.size())
+        {
+            print_error = true;
+        }
+
+        if (!print_error)
+        {
+            if (inputs[i].getType() != matlab::data::ArrayType::DOUBLE ||
+                inputs[i].getType() == matlab::data::ArrayType::COMPLEX_DOUBLE ||
+                inputs[i].getNumberOfElements() != 1) {
+                print_error = true;
+            }
+        }
+
+
+        if (print_error)
+        {
+            // Prepare string
+            err2print = tools::string::print2string("Input %d must be of type double", i + 1);
+
+            // Show string
+            std::fprintf(stderr, "ERROR: %s\n", err2print.c_str());
+
+            // Reset print error
+            print_error = false;
+        }
+    }
+
+    // Check arguments
+    for (auto & i : inputs2check.int2check)
+    {
+        // Safety check
+        if (i >= inputs.size())
+        {
+            print_error = true;
+        }
+
+        if (!print_error)
+        {
+            if (inputs[i].getType() != matlab::data::ArrayType::INT16 ||
+                inputs[i].getType() == matlab::data::ArrayType::COMPLEX_INT16 ||
+                inputs[i].getNumberOfElements() != 1) {
+                print_error = true;
+            }
+        }
+
+
+        if (print_error)
+        {
+            // Prepare string
+            err2print = tools::string::print2string("Input %d must be of type int16", i + 1);
+
+            // Show error
+            std::fprintf(stderr, "ERROR: %s\n", err2print.c_str());
+
+            // Reset print error
+            print_error = false;
+        }
+    }
+
+    // Check arguments
+    for (auto & i : inputs2check.str2check)
+    {
+        // Safety check
+        if (i >= inputs.size())
+        {
+            print_error = true;
+        }
+
+        if (!print_error)
+        {
+            if (inputs[i].getType() != matlab::data::ArrayType::MATLAB_STRING ||
+                inputs[i].getNumberOfElements() != 1) {
+                print_error = true;
+            }
+        }
+
+
+        if (print_error)
+        {
+            // Prepare string
+            err2print = tools::string::print2string("Input %d must be of type MATLAB_STRING", i + 1);
+
+            // Show error
+            std::fprintf(stderr, "ERROR: %s\n", err2print.c_str());
+
+            // Reset print error
+            print_error = false;
+        }
+    }
+
+    // Check number of outputs
+    if (outputs.size() > 1) {
+        std::fprintf(stderr, "ERROR: Only one output is returned\n");
+    }
+}
+*/
--- src/core/tools/mex_aux.h
+++ src/core/tools/mex_aux.h
@@ -16,9 +16,51 @@
 
 // Project libraries
 #include "tools/str.h"
+#include "base/enums.h"
 
 namespace mex_aux
 {
+    class inputs {
+    public:
+        inputs() = default;
+        ~inputs() = default;
+
+        // Define vectors to check the inputs
+        [[maybe_unused]] std::vector<int> vectors2check6 = {};
+        std::vector<int> vectors2check3 = {};
+        std::vector<int> doubles2check = {};
+        std::vector<int> int2check = {};
+        std::vector<int> str2check = {};
+    };
+
+    class inputs_MEX_VSAOD : public inputs {
+    public:
+        inputs_MEX_VSAOD(){init(); };
+        ~inputs_MEX_VSAOD() = default;
+
+        void init()
+        {
+            // Define vectors to check the inputs
+            this->vectors2check6 = {0, 1};
+            this->vectors2check3 = {2};
+            this->doubles2check = {3, 4};
+            this->int2check = {5, 6};
+            this->str2check = {7};
+        }
+    };
+    class inputs_MEX_GET_DA : public inputs {
+    public:
+        inputs_MEX_GET_DA() {init();};
+        ~inputs_MEX_GET_DA() = default;
+
+        void init()
+        {
+            // Define vectors to check the inputs
+            this->vectors2check6 = {0, 1};
+            this->doubles2check = {2};
+        }
+    };
+
     std::vector<double>
     convertMatlabTypedArray2NormalVector(const matlab::data::TypedArray<double> &array2convert);
 
@@ -34,6 +76,10 @@ namespace mex_aux
 
     template <typename T>
     void fromUTF8(const std::string& source, std::basic_string<T,std::char_traits<T>, std::allocator<T>>& result);
+
+    // void checkArguments(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs, MEX_FILE_TYPE type);
+    std::vector<std::string>
+    convertMatlabStrVector2NormalStrVector(const matlab::data::TypedArray<matlab::data::MATLABString> &str2convert);
 }
 
 // Include template
