6ec549d64da36b1acdb7a817eefeb32187d7c5c5
Bryan Cabrera <bryangcr25@gmail.com>
2024-01-23 17:26:26 +0100

    Example file from matlab.

--- src/main/matlab/propagation/propagation.m
+++ src/main/matlab/propagation/propagation.m
@@ -17,13 +17,23 @@ state_ini = [state_ini_dim(1:3) / scaling_length, state_ini_dim(4:6) / scaling_l
 stddev_dim = [10.0, 100.0, 0.0, 0.0, 0.0, 0.0437];
 stddev = [stddev_dim(1:3) / scaling_length, stddev_dim(4:6) / scaling_length];
 ci = 3;
+betas = ci * stddev;
 
 % Get DA vector
-state_DA_ini = mex_get_DA_state(state_ini, stddev, ci);
+state_DA_ini = mex_get_DA_state(state_ini, betas);
 fprintf("%s", string(state_DA_ini))
 
+% Propagation time
+t0 = 0.0;
+tf = 1000;
+dt = 10;
+t = [t0, tf, dt] / scaling_time;
+
+% NLI
+nli = 0.02;
+
 % Propagate DA vector
-mex_propagate_DA_state(state_DA_ini)
+state_DA_fin = mex_propagate_DA_state(state_DA_ini, betas, t, nli);
 
 
 
