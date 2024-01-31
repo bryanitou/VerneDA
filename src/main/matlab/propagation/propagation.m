56e42ae20c50f49d9df78567867c7f3eb1d231a6
Bryan Cabrera <bryangcr25@gmail.com>
2024-01-31 18:18:36 +0100

    Enhanced script, remaining filter.

--- src/main/matlab/propagation/propagation.m
+++ src/main/matlab/propagation/propagation.m
@@ -1,9 +1,10 @@
 % Clear everything
-bdclose all; % clear all libraries out of memory ( supposedly )
-clear all;   % clear all workspace variables, mex, etc. ( supposedly )
-close all;   % Close all windows
-rehash;      % cause all .m files to be reparsed when invoked again
+% bdclose all; % clear all libraries out of memory ( supposedly )
+% clear all;   % clear all workspace variables, mex, etc. ( supposedly )
+% close all;   % Close all windows
+% rehash;      % cause all .m files to be reparsed when invoked again
 % Set environment
+pause on;
 env;
 
 % Orbit Period
@@ -20,46 +21,59 @@ stddev = [stddev_dim(1:3) / scaling_length, stddev_dim(4:6) / scaling_velocity];
 ci = 3;
 betas = ci * stddev;
 
-% Get DA vector
-state_DA_ini = mex_get_DA_state(state_ini, betas);
-fprintf("%s", string(state_DA_ini))
-
 % Propagation time
 t0 = 0.0;
-tf = orbit_period_sec*0.5;
+tf = orbit_period_sec*0.2;
 dt = 10;
-t = [t0, tf, dt] / scaling_time;
+time = [t0, tf, dt] / scaling_time;
 
 % NLI
 nli = 0.02;
 
-% Propagate DA vector
-state_DA_fin = mex_propagate_DA_state(state_DA_ini, betas, t, nli);
-
 % Get samples after propagation
 n_samples = 10000;
-samples = mex_get_samples(state_DA_ini, state_DA_fin, stddev, n_samples);
-samples = mex_propagate_DA_and_get_samples(state_DA_ini, stddev, betas, t, nli, n_samples);
-
-% Dimensionalize result
-pos_samples = samples(1:3, :) * scaling_length;
-vel_samples = samples(4:6, :) * scaling_velocity;
-
-% Plot the result 
-plot3(pos_samples(1,:), pos_samples(2,:), pos_samples(3,:), '*');
-hold on;
-
-r_earth = 6378;
-[x ,y , z ] = sphere;
-x = x * r_earth;
-y = y * r_earth;
-z = z * r_earth;
-hold on;
-surf (x ,y , z ) ;
-axis equal ;
-xlabel (" x [ km ]") ;
-ylabel (" y [ km ]") ;
-zlabel (" z [ km ]") ;
-grid on;
 
 
+% Set initial state
+state_dim = state_ini_dim;
+state = state_ini;
+
+% Sensors
+state_sens = [0];
+
+while 1
+    % Get DA vector
+    state_DA = mex_get_DA_state(state, betas);
+    % fprintf("%s", string(state_DA_ini))
+
+    nx_state_DA = mex_propagate_DA_state(state_DA, betas, time, nli);
+    samples = mex_propagate_DA_and_get_samples(state_DA, stddev, betas, time, nli, n_samples);
+    
+    % Filter goes here
+    % TODO: Workaraound
+    pos_samples = samples(1:3, :) * scaling_length;
+    vel_samples = samples(4:6, :) * scaling_velocity;
+    
+    % Set next state
+    state = [samples(1:3, end)',samples(4:6, end)'];
+    state_dim = [state(1:3) * scaling_length, state(4:6)*scaling_velocity];
+
+    % Get experiment
+    simulator;
+
+    % Acquired position
+    state_sens = [mission.Satellite.TimeseriesPosECEF.Data(end, :)',
+        mission.Satellite.TimeseriesVelECEF.Data(end, :)'];
+    state_sens = state_sens / 1000; % Pass to km and km/s
+
+    % Add some noise
+    state_sens_noise = [state_sens(1:3) + randn(1, 1000)*1.5, % km
+                        state_sens(4:6) + randn(1, 1000)*1.5];% km/s
+
+    % Plot to see what's happening
+    close all;
+    plot_orbits;
+    plot_noise;
+
+    pause(2);
+end
